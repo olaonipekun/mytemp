5c644bbc9ac96543ccd5a52ac1f58782546b16c4
olaonipekunololade94 <olaonipekunololade94@gmail.com>
2024-05-26 17:56:20 +0000

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { useState, useEffect } from 'react'
+
+
 
 function App() {
+  const [color, setColor] = useState('')
+  const click = color => {
+    color = prompt('Enter your colors')
+    setColor(color)
+  }
+  useEffect(() => {
+    document.body.style.backgroundColor = color
+  }, [color])
+  
   return (
     <>
       <h1> hello world </h1>
-      <h2> Internet refresh is enabled </h2>
+      <h2> Internet Autorefresh Is Enabled </h2>
+      <button onClick={() => click(color)}>
+        change color
+      </button>   
     </>
   )
 }
