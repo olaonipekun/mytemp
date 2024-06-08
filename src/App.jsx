7ac171d23fcbf7c8b2081ebbefd0b734d1adde2c
olaonipekunololade94 <olaonipekunololade94@gmail.com>
2024-06-08 18:05:58 +0000

    push this to github

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,8 @@
-import './App.css'
-import { useState, useEffect } from 'react'
 
 
 
 function App() {
-  const [color, setColor] = useState('')
-  const click = color => {
-    color = prompt('Enter your colors')
-    setColor(color)
-  }
-  useEffect(() => {
-    document.body.style.backgroundColor = color
-  }, [color])
-  
-  return (
-    <>
-      <h1> hello world </h1>
-      <h2> Internet Autorefresh Is Enabled </h2>
-      <button onClick={() => click(color)}>
-        change color
-      </button>
-      <input type="text" id="myInput" />
-      
 
-    </>
-  )
 }
 
 export default App
---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+
+
+const Header = () => {
+  return (
+    <header>
+      <h1>My Temp</h1>
+    </header>
+  );
+};
+
+export default Header;
