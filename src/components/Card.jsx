26921cb69804fa8982038daa109a369b14372815
olaonipekunololade94 <olaonipekunololade94@gmail.com>
2024-06-11 05:39:31 +0000

    small changes to css

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,12 +1,14 @@
+
+
 function Card(){
     return(
         <div className="card">
-            <img className="card-img" src="https://picsum.photos/200/300" alt="A profile picture" />
-            <h2>Card Component</h2>
-            <p>I am Ololade and I code for a living </p>
-            <button className="btn">Click Me</button>
+            <img className="card-img" src="https://picsum.photos/200/250" alt="A profile picture"></img>
+            <h2 className="card-title">Bro Code</h2>
+            <p className="card-text">I am Ololade and I code for a living </p>
+            <button className="btnclick">Click Me</button>
         </div>
-    )
+    );
 }
 
 export default Card
