6f8685bdd1a24bdc907b30ea7cc6768d59b11db2
olaonipekunololade94 <olaonipekunololade94@gmail.com>
2024-06-18 06:53:54 +0000

    header css

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,25 +2,27 @@
 
 const Header = () => {
   return (
-    <header>
-      <h1>My Temp Website </h1>
-      <nav>
-        <ul>
-            <li>
-                <a href="#">Home</a>
-            </li>
-            <li>
-                <a href="#">About</a>
-            </li>
-            <li>
-                <a href="#">Services</a>
-            </li>
-            <li>
-                <a href="#">Contact</a>
-            </li>
-        </ul>
-      </nav>
-    </header>
+      <header>
+          <a href="/" className="logo-link">
+              <h1>My Temp Website </h1>
+          </a>
+          <nav>
+              <ul>
+                  <li>
+                      <a href="#">Home</a>
+                  </li>
+                  <li>
+                      <a href="#">About</a>
+                  </li>
+                  <li>
+                      <a href="#">Services</a>
+                  </li>
+                  <li>
+                      <a href="#">Contact</a>
+                  </li>
+              </ul>
+          </nav>
+      </header>
   );
 };
 
